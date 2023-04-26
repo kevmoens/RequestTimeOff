0cfd7f8533077d16e7982a2d6b923cdd51d2fe04
Kevin Moens <kevmoens@yahoo.com>
2023-04-25 21:43:20 -0500

    Fix Minimize Maximize Restore Close

--- RequestTimeOff/UserControls/MinMaxClose.xaml.cs
+++ RequestTimeOff/UserControls/MinMaxClose.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace RequestTimeOff.UserControls
 {
@@ -10,6 +12,30 @@ namespace RequestTimeOff.UserControls
         public MinMaxClose()
         {
             InitializeComponent();
+        }
+        private void CommandBinding_CanExecute_1(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+
+        private void CommandBinding_Executed_1(object sender, ExecutedRoutedEventArgs e)
+        {
+            SystemCommands.CloseWindow(Window.GetWindow(this));
+        }
+
+        private void CommandBinding_Executed_2(object sender, ExecutedRoutedEventArgs e)
+        {
+            if (Window.GetWindow(this).WindowState == WindowState.Maximized)
+            {
+                SystemCommands.RestoreWindow(Window.GetWindow(this));
+                return;
+            }
+            SystemCommands.MaximizeWindow(Window.GetWindow(this));
+        }
+
+        private void CommandBinding_Executed_3(object sender, ExecutedRoutedEventArgs e)
+        {
+            SystemCommands.MinimizeWindow(Window.GetWindow(this));
         }
     }
 }
